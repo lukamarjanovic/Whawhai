3e723b79fa7260acdbf7971156aaa0b829499612
Luka Marjanovic <lukaosk1@gmail.com>
2020-07-21 00:11:21 +0200

    Final update (hopefully)

--- Source/JS/home_page.js
+++ Source/JS/home_page.js
@@ -105,7 +105,27 @@ function onloadFunc(){
 						"WarriorType": warrType, 
 						"Attacks":[parseInt(attack0, 10), parseInt(attack1, 10), parseInt(attack2, 10)]};
 
-	attackSelect(warriorObj.WarriorType, warriorObj.Attacks);
+	var array = shuffle(warriorObj.Attacks);
+	attackSelect(warriorObj.WarriorType, array);
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 function JSONpostRegister(name, warriorType, attacks){
